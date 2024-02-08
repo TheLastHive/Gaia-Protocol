273baa7831e37d31f8ebd8772b5a6217ee1d84b5
Juan <juangonzalezfernandez004@alumnos.ilernasevilla.com>
2024-02-08 21:31:27 +0100

    Pools vista

--- Gaia-Protocol/routes/web.php
+++ Gaia-Protocol/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\PoolController;
 use App\Http\Controllers\TokenController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,20 +22,26 @@ Route::get('/', function () {
 });
 
 //rutas para pools
-Route::get('/addLiquiditySuccess', 'PoolController@addLiquiditySuccess')->name('addLiquiditySuccess');
-Route::get('/removeLiquidityError', 'PoolController@removeLiquidityError')->name('removeLiquidityError');
-Route::get('/removeLiquiditySuccess', 'PoolController@removeLiquiditySuccess')->name('removeLiquiditySuccess');
-Route::get('/createPoolSuccess', 'PoolController@createPoolSuccess')->name('createPoolSuccess');
-Route::get('/deletePoolSuccess', 'PoolController@deletePoolSuccess')->name('deletePoolSuccess');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/pools', [PoolController::class, 'showAllPools'])->name('pools.showAll');
+    Route::get('/mypools', [PoolController::class, 'showMyPools'])->name('pools.showMy');
+    Route::post('/addLiquidity', [PoolController::class, 'addLiquidity'])->name('addLiquidity');
+    Route::post('/removeLiquidity', [PoolController::class, 'removeLiquidity'])->name('removeLiquidity');
+    Route::post('/createPool', [PoolController::class, 'createPool'])->name('createPool');
+    Route::delete('/deletePool/{poolId}', [PoolController::class, 'deletePool'])->name('deletePool');
+});
 
 // Inicio tras login
 Route::get('/home', function () {
     return view('auth.dashboard');
 })->middleware(['auth', 'verified']);
 
+//! si se quiere añadir mas variables como tokens a la vista home se crea un metodo en el controlador home que extienda
+//! todos los metodos con las variables necesarias
+Route::get('/home', [PoolController::class, 'showHomePools'])->middleware(['auth', 'verified']);
+
 // Rutas que acceden solo desde login
 Route::prefix('')->middleware('auth', 'verified')->group(function () {
-
 });
 
 Route::post('/tokens/create', [TokenController::class, 'createToken'])->name('createToken');
