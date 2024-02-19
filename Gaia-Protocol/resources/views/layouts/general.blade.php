7873682451ef38419997b1ef2251d1343ddc0b81
DanielNV002 <nonprivado@gmail.com>
2024-02-19 11:03:14 +0100

    commit- desayuno

--- Gaia-Protocol/resources/views/layouts/general.blade.php
+++ Gaia-Protocol/resources/views/layouts/general.blade.php
@@ -2,13 +2,13 @@
 
 @section('general')
     {{-- SIDEBAR --}}
-    <div class="sidebar col-1 bg-warning rounded-5 d-grid justify-content-center align-items-center">
-        <a><button class="btn rounded-5 btn-secondary mt-5">A</button></a>
+    <div class="sidebar col-1 bg-warning rounded-5 d-grid justify-content-center align-items-center me-3">
+        <a><button class="btn rounded-5 btn-secondary mt-2">A</button></a>
         <a><button class="btn rounded-5 btn-secondary ">A</button></a>
         <a><button class="btn rounded-5 btn-secondary">A</button></a>
         <a><button class="btn rounded-5 btn-secondary">A</button></a>
         <a><button class="btn rounded-5 btn-secondary">A</button></a>
-        <a><button class="btn rounded-5 btn-secondary mb-5">A</button></a>
+        <a><button class="btn rounded-5 btn-secondary mb-2">A</button></a>
     </div>
 
     {{-- Vistas principales --}}
--- Gaia-Protocol/resources/views/layouts/template.blade.php
+++ Gaia-Protocol/resources/views/layouts/template.blade.php
@@ -36,55 +36,58 @@
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-warning shadow-sm d-flex justify-content-between pe-3">
-            <div class="">
-                <div class="sidebar2 d-flex">
-                    <a><button class="btn rounded-5 btn-secondary">A</button></a>
-                    <a><button class="btn rounded-5 btn-secondary ">A</button></a>
-                    <a><button class="btn rounded-5 btn-secondary">A</button></a>
-                    <a><button class="btn rounded-5 btn-secondary">A</button></a>
-                    <a><button class="btn rounded-5 btn-secondary">A</button></a>
-                    <a><button class="btn rounded-5 btn-secondary">A</button></a>
-                </div>
+        <nav class="navbar navbar-expand-md navbar-light bg-warning shadow-sm d-flex justify-content-between px-3 py-2">
+          
+                <div class="sidebar2 d-flex flex-grow-1">
+                    
+                    <div class="d-flex">
+                        <a><button class="btn rounded-5 btn-secondary me-1">A</button></a>
+                        <a><button class="btn rounded-5 btn-secondary me-1">A</button></a>
+                        <a><button class="btn rounded-5 btn-secondary me-1">A</button></a>
+                        <a><button class="btn rounded-5 btn-secondary me-1">A</button></a>
+                        <a><button class="btn rounded-5 btn-secondary me-1">A</button></a>
+                        <a><button class="btn rounded-5 btn-secondary me-1">A</button></a>
+                    </div>
 
-                <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
+                    <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav">
+                            <!-- Authentication Links -->
+                            @guest
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
-                            @endif
-                        @else
-                            {{-- @if (Auth::user()->email_verified_at) --}}
-                            <div class="dropdown flex-row-reverse">
-                                <button class="btn btn-secondary rounded-5" type="button" id="dropdownMenuButton1"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    {{ Auth::user()->name }}
-                                    <i class="fa-solid fa-user"></i>
-                                </button>
-                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
-                                    <li><a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                            @else
+                                {{-- @if (Auth::user()->email_verified_at) --}}
+                                <div class="dropdown flex-row-reverse">
+                                    <button class="btn btn-secondary rounded-5" type="button" id="dropdownMenuButton1"
+                                        data-bs-toggle="dropdown" aria-expanded="false">
+                                        {{ Auth::user()->name }}
+                                        <i class="fa-solid fa-user"></i>
+                                    </button>
+                                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
+                                        <li><a class="dropdown-item" href="{{ route('logout') }}"
+                                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
                                         </li>
-                                </ul>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                                {{-- @endif --}}
-                            </div>
-                        @endguest
-                    </ul>
+                                    </ul>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                    {{-- @endif --}}
+                                </div>
+                            @endguest
+                        </ul>
+                    </div>
                 </div>
-            </div>
+          
         </nav>
 
-        <main class="pt-4 ms-2 me-2 d-grid d-sm-flex">
+        <main class="pt-3 px-3 d-grid d-sm-flex">
             @yield('general')
         </main>
     </div>
